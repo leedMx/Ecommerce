15cf9c573e9d1855c92932dfaa1b8d714a4a988d
leedMx <esparza.mexico@gmail.com>
2021-03-27 02:24:59 -0600

    Serving real JWT

--- src/main/java/com/example/demo/security/LoginEndPointFilter.java
+++ src/main/java/com/example/demo/security/LoginEndPointFilter.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.demo.model.persistence.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @RequiredArgsConstructor
 public class LoginEndPointFilter extends UsernamePasswordAuthenticationFilter {
@@ -37,7 +40,15 @@ public class LoginEndPointFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        String jwt = "j.w.t";
+        String username = getUser(authResult).getUsername();
+        String jwt = JWT.create()
+                .withSubject(username)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+                .sign(Algorithm.HMAC512("SECRET"));
         response.addHeader("Authorization", "Bearer " + jwt);
+    }
+
+    private org.springframework.security.core.userdetails.User getUser(Authentication authResult) {
+        return (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
     }
 }
